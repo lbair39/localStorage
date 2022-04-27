64d50c73cea8d822d767bcb286825bb9123f839b
lbair39 <lilybair24@g.crookcountyschools.org>
2022-04-27 10:06:20 -0700

    made array

---
+++ app.js
@@ -0,0 +1,18 @@
+'use script'
+
+const myArray = ['milk', 'coffee', 'tea', 'soda', 'water'];
+
+    console.log (myArray);
+
+const textArray = JSON.stringify(myArray);
+
+    console.log(textArray);
+
+localStorage.setItem("storedArray", textArray);
+    console.log(localStorage);
+
+let unstoredArray = localStorage.getItem ("storedArray");
+
+let newArray = JSON.parse(unstoredArray);
+document.write(newArray[3]);
+
