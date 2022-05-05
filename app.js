6928a68997e94a638f63da9a5b49a7925e7078d5
lbair39 <lilybair24@g.crookcountyschools.org>
2022-05-05 09:14:36 -0700

    made changes

--- app.js
+++ app.js
@@ -1,18 +1,47 @@
-'use script'
+// 'use script'
 
-const myArray = ['milk', 'coffee', 'tea', 'soda', 'water'];
+// const myArray = ['milk', 'coffee', 'tea', 'soda', 'water'];
 
-    console.log (myArray);
+//     console.log (myArray);
 
-const textArray = JSON.stringify(myArray);
+// const textArray = JSON.stringify(myArray);
 
-    console.log(textArray);
+//     console.log(textArray);
 
-localStorage.setItem("storedArray", textArray);
-    console.log(localStorage);
+// localStorage.setItem("storedArray", textArray);
+//     console.log(localStorage);
 
-let unstoredArray = localStorage.getItem ("storedArray");
+// let unstoredArray = localStorage.getItem ("storedArray");
 
-let newArray = JSON.parse(unstoredArray);
-document.write(newArray[3]);
+// let newArray = JSON.parse(unstoredArray);
+// document.write(newArray[3]);
 
+
+
+const pet = {
+    species: "Dog",
+    breed: "German Shepherd",
+    name: "Duke",
+}
+  console.log(pet);
+  let petString = JSON.stringify(pet);
+  console.log(petString);
+  localStorage.setItem("storedPet", petString);
+  let unstoredPet = localStorage.getItem("storedPet");
+  let myPet = JSON.parse(unstoredPet);
+  console.log(myPet);
+  const myNewPet = "My new pet is a " + myPet.species + ". He is a " + myPet.breed + ", And his name is " + myPet.name + "!";
+  const section = document.getElementById("mainSection");
+  const h2elm = document.createElement('h2');
+  h2elm.textContent = myNewPet;
+  section.appendChild(h2elm);
+  
+  
+  const animals = ["Cat", "Fish", "Lion", "Wolf", "Otter", "Seal", "Bear"];
+  let animalsString = JSON.stringify(animals);
+  localStorage.setItem("myArray", animalsString);
+  let unstoredArray = localStorage.getItem("myArray");
+  let newAnimals = JSON.parse(unstoredArray);
+  const h3elm = document.createElement('h3');
+  h3elm.textContent = "My favorite animal is: " + newAnimals[6];
+  section.appendChild(h3elm);
